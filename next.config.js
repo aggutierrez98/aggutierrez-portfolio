5ed3e60895cba295a5c28e8ab42f71d9591daff0
Agustin Gutierrez <agustinguti123@gmail.com>
2023-05-19 16:09:05 -0300

    Feat: Added nextjs-bundle-analyzer and changed husky config

--- next.config.js
+++ next.config.js
@@ -7,18 +7,24 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withPWA({
-  poweredByHeader: false,
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-  reactStrictMode: true,
-  output: "standalone",
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
+
+module.exports = withPWA(
+  withBundleAnalyzer({
+    poweredByHeader: false,
+    images: {
+      domains: ["res.cloudinary.com"],
+    },
+    reactStrictMode: true,
+    output: "standalone",
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+      return config;
+    },
+  })
+);
